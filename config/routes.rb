dced2ed3d1645a836960c221cdaedf0aaa8ce73f
Irfan Fadilah <irfan@41studio.com>
2016-01-31 18:36:22 +0700

    update edit user page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   # Devise
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
 
   # Pages
   get 'home', to: 'pages#home'
